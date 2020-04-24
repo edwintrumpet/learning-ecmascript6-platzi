dc4ceeffe164b7422963cc30e44b8c0a93f7f71b
Edwin García <spark.com.co@gmail.com>
2020-04-23 19:09:20 -0500

    concatenation

---
+++ src/es6/template-literals.js
@@ -0,0 +1,9 @@
+const firstWord = 'Hello';
+const secondWord = 'World';
+
+const oldConcat = firstWord + ' ' + secondWord;
+
+const usingTemplateLiterals = `${firstWord} ${secondWord}`;
+
+console.log(oldConcat);
+console.log(usingTemplateLiterals);
