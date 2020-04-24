552b86898789ff2dff9591d08c57226d4a30b79b
Edwin García <spark.com.co@gmail.com>
2020-04-23 20:45:25 -0500

    destructuring

---
+++ src/es6/destructuring.js
@@ -0,0 +1,11 @@
+const person = {
+    "name": "edwin",
+    "age": 32,
+    "country": "COL"
+};
+
+console.log(person.name, person.age, person.country);
+
+const { name, age, country } = person;
+
+console.log(name, age, country);
