6638f3282ab4600fc43521ab89784fded679b970
Edwin García <spark.com.co@gmail.com>
2020-04-24 16:54:23 -0500

    object entries values

---
+++ src/es8/object-entries-values.js
@@ -0,0 +1,15 @@
+const data = {
+    frontend: 'Óscar',
+    backend: 'Isabel',
+    design: 'Ana'
+}
+
+const entries = Object.entries(data)
+
+console.log(entries)
+console.log(entries.length)
+
+
+const values = Object.values(data)
+
+console.log(values)
