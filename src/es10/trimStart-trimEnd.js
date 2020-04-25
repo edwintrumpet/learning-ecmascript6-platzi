2df3ad4478b04e2b99effa227f2e800f98265619
Edwin García <spark.com.co@gmail.com>
2020-04-24 22:05:12 -0500

    trimStart and trimEnd

---
+++ src/es10/trimStart-trimEnd.js
@@ -0,0 +1,10 @@
+const greeting1 = '       hello world'
+
+console.log(greeting1)
+console.log(greeting1.trimStart())
+
+
+const greeting2 = 'hello world       '
+
+console.log(greeting2)
+console.log(greeting2.trimEnd())
